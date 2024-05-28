42b9a63563073c44501923c6c8145cc65a3c50d5
david hopkinson <hopkinson.dave@yahoo.co.uk>
2024-05-28 13:04:32 +0100

    added 1 user 1 dog seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+
+user_1 = User.create(
+  email: "Jordan@email.com",
+  first_name: "Jordan",
+  last_name: "den Dulk",
+  phone_number: "123456",
+  address: "le wagon"
+)
+
+dog_1 = Dog.new(
+  name: "Frankie",
+  age: "5",
+  breed: "Cava-poo",
+  description: "lovely 🐶",
+  user: user_1
+)
+
+file = URI.open("https://dogtime.com/wp-content/uploads/sites/12/2023/07/GettyImages-1330546852.jpg?w=1024")
+dog_1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+dog_1.save
