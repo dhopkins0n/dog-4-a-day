b6e8db0c2096aad6225559a7ae10115fa7a923da
david hopkinson <hopkinson.dave@yahoo.co.uk>
2024-05-28 13:53:48 +0100

    added a few extra seeds and added images to user create and edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,32 @@
 #   end
 require "open-uri"
 
-user_1 = User.create(
+
+Dog.destroy_all
+User.destroy_all
+
+user_1 = User.create!(
   email: "Jordan@email.com",
   first_name: "Jordan",
   last_name: "den Dulk",
   phone_number: "123456",
-  address: "le wagon"
+  address: "le wagon",
+  password: "123456"
 )
+
+user_2 = User.new(
+  email: "test@test.com",
+  first_name: "Shaggy",
+  last_name: "Doo",
+  phone_number: "123456",
+  address: "Mystery Van",
+  password: "123456"
+
+)
+
+file = URI.open("https://i.ebayimg.com/images/g/MCoAAOSwhdhicr~Q/s-l1200.jpg")
+user_2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+user_2.save!
 
 dog_1 = Dog.new(
   name: "Frankie",
@@ -27,4 +46,29 @@ dog_1 = Dog.new(
 
 file = URI.open("https://dogtime.com/wp-content/uploads/sites/12/2023/07/GettyImages-1330546852.jpg?w=1024")
 dog_1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-dog_1.save
+dog_1.save!
+
+
+dog_2 = Dog.new(
+  name: "Scooby",
+  age: "50",
+  breed: "great dane",
+  description: "strange mutant talking dog",
+  user: user_2
+)
+
+file = URI.open("https://static.wikia.nocookie.net/near-pure-good-hero/images/d/db/Scooby-Doo_transparent.png/revision/latest?cb=20230713015847")
+dog_2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+dog_2.save!
+
+dog_3 = Dog.new(
+  name: "Scrappy",
+  age: "30",
+  breed: "great dane",
+  description: "small and aggressive",
+  user: user_2
+)
+
+file = URI.open("https://cdn.hanna-barberawiki.com/thumb/b/b6/Scrappy-Doo.png/1200px-Scrappy-Doo.png")
+dog_2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+dog_2.save!
