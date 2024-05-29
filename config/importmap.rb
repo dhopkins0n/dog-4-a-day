aad3e4a331e41ec8dee83b02a873bff5a326e3c7
david hopkinson <hopkinson.dave@yahoo.co.uk>
2024-05-29 16:06:07 +0100

    added dropdowns to the dashboard

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,11 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr" # @4.6.13
+
+pin "stimulus-use" # @0.52.2
