fa4fd6719042b71a7d7d8f1938a95197b34642e2
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-05-30 12:15:26 +0100

    added images gallery

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,6 +6,7 @@ class Dog < ApplicationRecord
   validates :breed, presence: true, length: { maximum: 20 }
   validates :age, presence: true, numericality: { only_integer: true }
   validates :description, length: { maximum: 500 }
-  
+
   has_one_attached :photo
+  has_many_attached :images
 end
