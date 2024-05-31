f7ecfefef64831b351233689690cfda87e33a515
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-05-31 11:49:30 +0100

    redirect to dogs page if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :redirect_if_logged_in
   def home
     @dogs = Dog.all
   end
 
   def dashboard
     @dogs = Dog.all
+  end
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to dogs_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "dashboard", to: "pages#dashboard", as: :dashboard
-
 
   resources :dogs do
     resources :bookings, only: [:create, :edit, :update, :destroy]
